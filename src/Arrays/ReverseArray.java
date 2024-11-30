bb6c5c18074b244b34dac4ea269febd42d0eb21f
Barun Panthi Sharma <barunpanthisharma11@gmail.com>
2024-11-30 15:57:23 +0545

    Reversing the array without using 3rd variable

--- src/Arrays/ReverseArray.java
+++ src/Arrays/ReverseArray.java
@@ -1,2 +1,26 @@
-package Arrays;public class ReverseArray {
+package Arrays;
+
+import java.util.Arrays;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 0, 5, 9};
+
+        reverse(arr);
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+    static void reverse(int[] arr){
+
+        int start = 0;
+        int end = arr.length-1;
+            while(start < end) {
+                arr[start] = arr[start] + arr[end];
+                arr[end] = arr[start] - arr[end];
+                arr[start] = arr[start] - arr[end];
+                start ++;
+                end--;
+        }
+    }
 }
