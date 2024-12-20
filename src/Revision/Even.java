3b5ef6ad885e3c4b6e0ff1b59aa1ddec14956978
Barun Panthi Sharma <barunpanthisharma11@gmail.com>
2024-12-20 08:31:39 +0545

    Find how many even numbes are in array

---
+++ src/Revision/Even.java
@@ -0,0 +1,22 @@
+package Revision;
+
+import java.util.Arrays;
+
+public class Even {
+    static int findEvenInArray(int[] arr ){
+        int num = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] % 2 == 0) {
+               num ++;
+            }
+
+        }
+        return num;
+    }
+    public static void main(String[] args) {
+        int[] arr = {2,3,4,1,6};
+        int result = findEvenInArray(arr);
+        System.out.println("There are " + result + " even numbers in array.gi");
+
+    }
+}
