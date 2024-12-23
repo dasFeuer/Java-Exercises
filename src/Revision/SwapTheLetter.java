8d5bd11ad8f3e1c18e9f863212d40f60319330d1
Barun Panthi Sharma <barunpanthisharma11@gmail.com>
2024-12-23 20:29:33 +0545

    Swap letters

---
+++ src/Revision/SwapTheLetter.java
@@ -0,0 +1,15 @@
+package Revision;
+
+public class SwapTheLetter {
+    public static void main(String[] args) {
+        char[] letters = {'a', 'b'};
+        swapTheLetter(letters);
+        System.out.println("Swapped letters: " + letters[0] + " and " + letters[1]);
+    }
+    static public void swapTheLetter(char[] letter){
+        letter[0] = (char) (letter[0] + letter[1]);
+        letter[1] = (char) (letter[0] - letter[1]);
+        letter[0] = (char) (letter[0] - letter[1]);
+
+    }
+}
