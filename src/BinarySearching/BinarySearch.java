93b24c2579c72cfd1ab6e5f4d3f69429dd7d9e4b
Barun Panthi Sharma <barunpanthisharma11@gmail.com>
2025-01-08 19:32:23 +0545

    Binary Search

--- src/BinarySearching/BinarySearch.java
+++ src/BinarySearching/BinarySearch.java
@@ -4,30 +4,31 @@ import java.util.Scanner;
 
 public class BinarySearch {
     static int binarySearch(int[] arr, int targetNum){
-        int start = 0;
-        int end = arr.length - 1;
+        int startNum = 0;
+        int endNum = arr.length - 1;
 
         // To find the middle index
-        while (start <= end){
-            int middleNum = start + (end - start) / 2;
+        while (startNum <= endNum){
+            int middleNum = startNum + (endNum - startNum) / 2;
             if (targetNum < arr[middleNum]) {
-                end = middleNum - 1;
+                endNum = middleNum - 1;
             } else if (targetNum > arr[middleNum]) {
-                start = middleNum + 1;
+                startNum = middleNum + 1;
             } else {
                 return middleNum;
             }
         }
-        System.out.println("The number that you are searching for doesn't found at any index.");
+        System.out.println("The number you are searching for is not found.");
         return -1;
 
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] arr = {-22, -3, -1, 5, 7, 8, 9, 11, 15, 17, 19, 22, 24, 27, 31, 32, 35};
+        System.out.println("Enter the number to search:");
         int target = sc.nextInt();
 
-        System.out.println("Finding the index of the number with binary search......");
+        System.out.println("Finding the index of the number using binary search......");
         int answer = binarySearch(arr, target);
         System.out.println(answer);
     }
