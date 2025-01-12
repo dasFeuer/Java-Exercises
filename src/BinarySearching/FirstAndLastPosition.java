1f8941da2710ad8596f630128f8915e085b02abe
Barun Panthi Sharma <barunpanthisharma11@gmail.com>
2025-01-12 15:02:25 +0545

    Finding the FirstAndLastIndexPosition of the numbers using binary search

--- src/BinarySearching/FirstAndLastPosition.java
+++ src/BinarySearching/FirstAndLastPosition.java
@@ -7,21 +7,21 @@ public class FirstAndLastPosition {
 
     // Check for first occurrence if target first
 
-    static int[] searchRange(int[] nums, int target){
-        int[] ans = {-1, -1};
-        ans[0] = search(nums,target, true);
-        if(ans[0] != -1){
-            ans[1] = search(nums, target, false);
+    static int[] searchTheRange(int[] numbers, int targetNumber){
+        int[] answer = {-1, -1};
+        answer[0] = search(numbers, targetNumber, true);
+        if(answer[0] != -1){
+            answer[1] = search(numbers, targetNumber, false);
         }
-        return ans;
+        return answer;
     }
 
     // This function just returns the index value of target
 
-    static int search(int[] nums, int target, boolean findFirstIndex){
-        int ans = -1;
+    static int search(int[] numbers, int targetNumber, boolean findTheFirstIndex){
+        int answer = -1;
         int start = 0;
-        int end = nums.length - 1;
+        int end = numbers.length - 1;
 
         while (start <= end){
 
@@ -29,30 +29,30 @@ public class FirstAndLastPosition {
 
             int mid = start + (end - start) / 2;
 
-            if(target < nums[mid]){
+            if(targetNumber < numbers[mid]){
                 end = mid - 1;
-            } else if(target > nums[mid]){
+            } else if(targetNumber > numbers[mid]){
                 start = mid + 1;
             } else {
-                ans = mid;
-                if(findFirstIndex){
+                answer = mid;
+                if(findTheFirstIndex){
                     end = mid - 1;
                 } else {
                     start = mid + 1;
                 }
             }
         }
-        return ans;
+        return answer;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         //     index = 0, 1, 2, 3, 4
-        int[] nums = { 5, 7, 8, 8, 9};
+        int[] numbers = { 5, 7, 8, 8, 9};
         System.out.println("Enter the number to search:");
-        int target = sc.nextInt(); // target = 8, ans = [2, 3]
+        int targetNumber = sc.nextInt(); // targetNumber = 8, ans = [2, 3]
 
-        System.out.println("Finding the index of the number using binary search......");
-        int[] ans = searchRange(nums, target);
-        System.out.println(Arrays.toString(ans));
+        System.out.println("Finding the first and last index using binary search...");
+        int[] ans = searchTheRange(numbers, targetNumber);
+        System.out.println(Arrays.toString(ans)); // [2, 3]
     }
 }
