5f4a506820ea55ddff18013202a2e435c1d757ed
Barun Panthi Sharma <barunpanthisharma11@gmail.com>
2025-01-13 08:19:23 +0545

    findInMountainArray and peakIndexInMountainArray

---
+++ src/BinarySearching/Mountain.java
@@ -0,0 +1,25 @@
+package BinarySearching;
+
+
+public class Mountain {
+    public static int peakIndexInMountainArray(int[] arr){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end){
+            int mid = start + (end - start) / 2;
+
+            if(arr[mid] > arr[mid + 1]){
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+    public static void main(String[] args) {
+        int[] arr = {0,1,0};
+        int answer = peakIndexInMountainArray(arr);
+        System.out.println(answer);
+    }
+}
