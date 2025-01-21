2069129988a5b1a3dac80fbcc7ba513341afc6fa
Barun Panthi Sharma <barunpanthisharma11@gmail.com>
2025-01-21 07:00:52 +0545

    Code updated: continueAddedArray

--- src/Arrays/AddArray.java
+++ src/Arrays/AddArray.java
@@ -1,5 +1,7 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class AddArray {
     static int addedArray(int[]nums){
         int sum = 0;
@@ -7,6 +9,14 @@ public class AddArray {
             sum += nums[i];
         }
         return sum;
+    }
+    static int[] continueAddedArray(int[]nums){
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            nums[i] = sum;
+        }
+        return nums;
     }
 
 //    static int addedArray(int[]nums){
@@ -21,6 +31,8 @@ public class AddArray {
     public static void main(String[] args) {
         int[]nums = {2, 4, 5, 1, 7};
         int result = addedArray(nums);
+        int[] result2 = continueAddedArray(nums);
         System.out.println(result);
+        System.out.println(Arrays.toString(result2));
     }
 }
